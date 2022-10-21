30a2eedaecef1db54fdb480586385a16f08ed401
Pedro Tonini <51178173+ptonini@users.noreply.github.com>
2022-10-21 20:08:28 -0300

    feat: added support for secrets and configmaps as rootca sources

--- kac/config_test.go
+++ kac/config_test.go
@@ -1,12 +1,15 @@
 package kac
 
 import (
+	"context"
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func Test_Config(t *testing.T) {
+
+	ctx := context.Background()
 
 	t.Run("test read invalid file", func(t *testing.T) {
 		assert.Error(t, readConfig("../config2.yaml"))
@@ -17,22 +20,22 @@ func Test_Config(t *testing.T) {
 	})
 
 	t.Run("test valid config", func(t *testing.T) {
-		assert.NoError(t, fetchBundles())
+		assert.NoError(t, fetchBundles(ctx))
 	})
 
 	t.Run("test config with invalid bundle url", func(t *testing.T) {
 		viper.Set("rootCA.baltimore.source", "https://invalid.local")
-		assert.Error(t, fetchBundles())
+		assert.Error(t, fetchBundles(ctx))
 	})
 
 	t.Run("test config with invalid bundle content", func(t *testing.T) {
 		viper.Set("rootCA.baltimore.source", "https://example.com")
-		assert.Error(t, fetchBundles())
+		assert.Error(t, fetchBundles(ctx))
 	})
 
 	t.Run("test config with invalid local content", func(t *testing.T) {
 		viper.Set("rootCA.local.bundle", "invalid")
-		assert.Error(t, fetchBundles())
+		assert.Error(t, fetchBundles(ctx))
 	})
 
 }
