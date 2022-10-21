fc5a5a08d0d0c0c86b33c9a32e81ed4a6febf842
Pedro Tonini <51178173+ptonini@users.noreply.github.com>
2022-10-21 20:08:28 -0300

    feat: added support for secrets and configmaps as rootca sources

--- kac/config.go
+++ kac/config.go
@@ -21,10 +21,13 @@
 package kac
 
 import (
+	"context"
+	"encoding/base64"
 	"fmt"
 	"github.com/fsnotify/fsnotify"
 	"github.com/spf13/viper"
 	"io/ioutil"
+	"k8s.io/apimachinery/pkg/apis/meta/v1"
 	"log"
 	"net/http"
 	"path/filepath"
@@ -46,11 +49,12 @@ type Config struct {
 
 func LoadConfig(configFile string) {
 	log.Printf("Loding config")
+	ctx := context.Background()
 	err := readConfig(configFile)
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = fetchBundles()
+	err = fetchBundles(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -71,22 +75,33 @@ func readConfig(configFile string) error {
 	return viper.ReadInConfig()
 }
 
-func fetchBundles() error {
+func fetchBundles(ctx context.Context) error {
 	config, err := getConfig()
 	for k, v := range config.RootCA {
 		var bundle string
+		log.Printf("Fetching bundle %s from %s: %s", k, v.Type, v.Source)
 		switch v.Type {
+		case "local":
+			if err = validateCertificate(v.Bundle); err == nil {
+				bundle = v.Bundle
+			} else {
+				return err
+			}
 		case "url":
-			log.Printf("Fetching bundle %s from %s", k, v.Source)
 			bundle, err = getBundleFromURL(v.Source)
 			if err != nil {
 				return err
 			}
-		case "local":
-			log.Printf("Fetching bundle %s from local config", k)
-			if err = validateCertificate(v.Bundle); err == nil {
-				bundle = v.Bundle
-			} else {
+		case "secret":
+			s := strings.Split(v.Source, "/")
+			bundle, err = getCAFromSecret(ctx, s[0], s[1], s[2])
+			if err != nil {
+				return err
+			}
+		case "configMap":
+			s := strings.Split(v.Source, "/")
+			bundle, err = getCAFromConfigMap(ctx, s[0], s[1], s[2])
+			if err != nil {
 				return err
 			}
 		}
@@ -110,6 +125,26 @@ func getBundleFromURL(url string) (string, error) {
 	defer func() { _ = resp.Body.Close() }()
 	bodyStr := string(body)
 	return bodyStr, validateCertificate(bodyStr)
+}
+
+func getCAFromSecret(ctx context.Context, ns string, secretName string, key string) (string, error) {
+	clientSet, err := getKubernetesClientSet(ctx)
+	if err != nil {
+		return "", err
+	}
+	secret, _ := clientSet.CoreV1().Secrets(ns).Get(ctx, secretName, v1.GetOptions{})
+	certificate := base64.StdEncoding.EncodeToString(secret.Data[key])
+	return certificate, validateCertificate(certificate)
+}
+
+func getCAFromConfigMap(ctx context.Context, ns string, configMapName string, key string) (string, error) {
+	clientSet, err := getKubernetesClientSet(ctx)
+	if err != nil {
+		return "", err
+	}
+	configMap, _ := clientSet.CoreV1().ConfigMaps(ns).Get(ctx, configMapName, v1.GetOptions{})
+	certificate := configMap.Data[key]
+	return certificate, validateCertificate(certificate)
 }
 
 func validateCertificate(bundle string) error {
